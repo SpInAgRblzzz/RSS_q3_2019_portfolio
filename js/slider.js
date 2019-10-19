037a4a9d10d74594e384831bb16d8956e8f036c5
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-10-19 20:28:32 +0300

    feat: slider added
    extremly ugly commit(((

--- js/slider.js
+++ js/slider.js
@@ -36,3 +36,60 @@ buttonBack.addEventListener('click', goBack);
 const buttonFoward = document.querySelector('.slider__btn--foward');
 buttonFoward.addEventListener('click', goFoward);
 
+
+
+const mode = document.querySelector('.mode');
+
+sliderItems.forEach(function (item) {
+    const itemProject = item.querySelector('.slider__item-project');
+    itemProject.addEventListener('click', function () {
+        const placeholder = this.querySelector('.slider__project-placeholder');
+        placeholder.classList.add('inactive');
+
+        const project = this.querySelector('.slider__project-wrapper');
+        project.classList.remove('inactive');
+
+        const slider = document.querySelector('.slider');
+        slider.classList.add('project-mode');
+
+        buttonBack.classList.add('project-mode');
+        buttonFoward.classList.add('project-mode');
+
+        mode.classList.remove('inactive');
+    });
+});
+
+const modeBack = mode.querySelector('.mode__back');
+
+modeBack.addEventListener('click', function () {
+    const slider = document.querySelector('.slider');
+    slider.classList.remove('project-mode');
+
+    const currentActive = slider.querySelector('.active');    
+
+    const placeholder = currentActive.querySelector('.slider__project-placeholder');
+    placeholder.classList.remove('inactive');
+
+    const project = currentActive.querySelector('.slider__project-wrapper');
+    project.classList.add('inactive');    
+
+    buttonBack.classList.remove('project-mode');
+    buttonFoward.classList.remove('project-mode');
+
+    mode.classList.add('inactive');
+});
+
+const modeMobile = mode.querySelector('.mode__toggle');
+
+modeMobile.addEventListener('click',function(){
+    const slider = document.querySelector('.slider');
+
+    const currentActive = slider.querySelector('.active');
+    if(currentActive.classList.contains('mobile')){
+        currentActive.classList.remove('mobile');
+        modeMobile.innerHTML = 'MOBILE';
+    }else{
+        currentActive.classList.add('mobile');
+        modeMobile.innerHTML = 'DESKTOP';
+    }
+});
